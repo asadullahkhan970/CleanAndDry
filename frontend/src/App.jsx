7398b7d34a838d5dbbd5ac199eaeedfc706cb1d2
Asadullah-khan596 <asadullahkhan3096@gmail.com>
2024-11-19 14:00:50 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
@@ -22,12 +22,15 @@ const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
 function App() {
+  const [viewTerms, setViewTerms] = useState(false);
+
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [contactData, setContactData] = useState({
     name: "",
     phone: "",
     message: "",
   });
+
   const dropdownMenuRef = useRef(null);
   const navigate = useNavigate();
 
@@ -312,107 +315,108 @@ function App() {
 
         <div className="margin-div" id="about"></div>
         <div className="about-container">
-        <div className="about-box" id="image-box"></div>
-        <div className="about-box" id="aboutbox">
-          <h3 className="about-header">About us</h3>
-          <p>
-            Welcome to Clean &amp; Dry, where we bring quality, convenience, and
-            care to your doorstep with exceptional dry cleaning services.
-            Whether it's delicate fabrics, business attire, or everyday wear,
-            our expert team uses the latest eco-friendly techniques to ensure
-            your items come back looking and feeling their best. With our easy
-            online booking, you can schedule pickups and deliveries at times
-            that work for you, saving you time and hassle. We treat every item
-            with attention to detail, handling tough stains, delicate materials,
-            and special requests with precision and care. Trust us to make your
-            wardrobe look pristine and keep your items in top shape. Choose lean
-            &amp; Dry for dry cleaning you can depend on, every time.<br />
-            <a href="#conditions" id="conditions">Terms and Conditionss</a>
-          </p>
-        </div>
-      </div>
-
-          <div className="condition-container" id="condition">
-            <div className="conditions-box">
-              <div className="conditions-box-headers">
-                <h3 className="conditions-header">Terms and Conditions</h3>
-                <span id="closeicon" role="button" aria-label="Close">&times;</span>
-              </div>
-              <ol>
-                <li>Not responsible for the bleeding of colors or shrinkage of material and does not guarantee 100% stain removal.</li>
-                <li>Cannot be held responsible for breaking buttons, hooks, beads, trimming, or thread-outs.</li>
-                <li>Not liable for any delays, losses, or damages arising from unforeseen circumstances or events beyond control.</li>
-                <li>Any material exchange must be notified within 24 hours.</li>
-                <li>
-                  Customers are advised to measure the fabric at the time of delivery. Any deficiency at the time of delivery should be reported immediately.
-                </li>
-                <li>
-                  Customers are encouraged to inspect their material for any potential damage before delivery.
-                </li>
-              </ol>
-            </div>
-          </div>
-        </div>
-
-
-        <div className="margin-div" id="services"></div>
-
-        <div className="services-box">
-          <h3 className="services-header">Services</h3>
-          <ul>
-            <li>
-              <span>Collection & Pickup : </span><br />We carefully collect your
-              clothes from your doorstep for dry cleaning, ensuring safe handling.
-            </li>
-            <li>
-              <span>Gentle Washing with Purified Water : </span><br />Each fabric
-              is washed using purified water and Natural solutions to maintain
-              fabric quality and freshness.
-            </li>
-            <li>
-              <span>Machine washing : </span><br />The material is loaded into
-              machines for dry cleaning. These machines are very effective in
-              removing stains, marks, etc without damaging the materials.
-            </li>
-            <li>
-              <span>Professional Ironing : </span><br />After washing, our experts
-              meticulously press (iron) the clothes to give them a crisp, polished
-              look.
-            </li>
-            <li>
-              <span>Secure Packaging : </span><br />Once ironed, every fabric is
-              neatly packed in transparent covers to keep them safe and ready for
-              delivery.
-            </li>
-            <li>
-              <span>Timely Delivery : </span><br />We ensure prompt delivery of
-              your cleaned clothes, precisely on the date specified.
-            </li>
-          </ul>
-        </div>
-
-        <div className="client-box">
-          <div className="client-content">
-            <h3 className="client-header">Clients</h3>
+          <div className="about-box" id="image-box"></div>
+          <div className="about-box" id="aboutbox">
+            <h3 className="about-header">About us</h3>
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
-              reprehenderit veniam sequi, facilis autem fugiat et minima quidem
-              delectus repudiandae, illo vero hic minus laboriosam? Vitae magnam
-              quia aperiam iusto assumenda error, sit quidem labore quod nemo
-              accusantium non! Magnam quidem harum ipsum aliquam nostrum?
+              Welcome to Clean &amp; Dry, where we bring quality, convenience, and
+              care to your doorstep with exceptional dry cleaning services.
+              Whether it's delicate fabrics, business attire, or everyday wear,
+              our expert team uses the latest eco-friendly techniques to ensure
+              your items come back looking and feeling their best. With our easy
+              online booking, you can schedule pickups and deliveries at times
+              that work for you, saving you time and hassle. We treat every item
+              with attention to detail, handling tough stains, delicate materials,
+              and special requests with precision and care. Trust us to make your
+              wardrobe look pristine and keep your items in top shape. Choose lean
+              &amp; Dry for dry cleaning you can depend on, every time.<br />
+              <a onClick={() => { setViewTerms(true) }}  id="conditions">Terms and Conditionss</a>
             </p>
           </div>
         </div>
 
-        <div className="margin-div" id="location"></div>
-        <div className="location-page">
+        {viewTerms?<div className="condition-container" id="condition">
+          <div className="conditions-box">
+            <div className="conditions-box-headers">
+              <h3 className="conditions-header">Terms and Conditions</h3>
+              <span onClick={()=>{setViewTerms(false)}} id="closeicon" role="button" aria-label="Close">&times;</span>
+            </div>
+            <ol>
+              <li>Not responsible for the bleeding of colors or shrinkage of material and does not guarantee 100% stain removal.</li>
+              <li>Cannot be held responsible for breaking buttons, hooks, beads, trimming, or thread-outs.</li>
+              <li>Not liable for any delays, losses, or damages arising from unforeseen circumstances or events beyond control.</li>
+              <li>Any material exchange must be notified within 24 hours.</li>
+              <li>
+                Customers are advised to measure the fabric at the time of delivery. Any deficiency at the time of delivery should be reported immediately.
+              </li>
+              <li>
+                Customers are encouraged to inspect their material for any potential damage before delivery.
+              </li>
+            </ol>
+          </div>
+        </div>
+        :""
+      }
+    </div>
+
+      <div className="margin-div" id="services"></div>
+
+      <div className="services-box">
+        <h3 className="services-header">Services</h3>
+        <ul>
+          <li>
+            <span>Collection & Pickup : </span><br />We carefully collect your
+            clothes from your doorstep for dry cleaning, ensuring safe handling.
+          </li>
+          <li>
+            <span>Gentle Washing with Purified Water : </span><br />Each fabric
+            is washed using purified water and Natural solutions to maintain
+            fabric quality and freshness.
+          </li>
+          <li>
+            <span>Machine washing : </span><br />The material is loaded into
+            machines for dry cleaning. These machines are very effective in
+            removing stains, marks, etc without damaging the materials.
+          </li>
+          <li>
+            <span>Professional Ironing : </span><br />After washing, our experts
+            meticulously press (iron) the clothes to give them a crisp, polished
+            look.
+          </li>
+          <li>
+            <span>Secure Packaging : </span><br />Once ironed, every fabric is
+            neatly packed in transparent covers to keep them safe and ready for
+            delivery.
+          </li>
+          <li>
+            <span>Timely Delivery : </span><br />We ensure prompt delivery of
+            your cleaned clothes, precisely on the date specified.
+          </li>
+        </ul>
+      </div>
+
+      <div className="client-box">
+        <div className="client-content">
+          <h3 className="client-header">Clients</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
+            reprehenderit veniam sequi, facilis autem fugiat et minima quidem
+            delectus repudiandae, illo vero hic minus laboriosam? Vitae magnam
+            quia aperiam iusto assumenda error, sit quidem labore quod nemo
+            accusantium non! Magnam quidem harum ipsum aliquam nostrum?
+          </p>
+        </div>
+      </div>
+
+      <div className="margin-div" id="location"></div>
+      <div className="location-page">
         <div className="map-section">
           <div className="titles">
             <h2>Location</h2>
           </div>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30443.191613276485!2d78.3635217793352!3d17.48845898859954!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb9226c6276feb%3A0x4dc887d1d8cf70a2!2sHyderabad%2C%20Telangana%20500085!5e0!3m2!1sen!2sin!4v1729839734195!5m2!1sen!2sin"
-            style={{border: "0"}}
+            style={{ border: "0" }}
             height="60%"
             width="100%"
             allowfullscreen=""
@@ -435,50 +439,50 @@ function App() {
         </div>
       </div>
 
-        <div className="margin-div" id="contact"></div>
-        <div className="contact-container">
-          <h1 className="contact-heading">Contact Us</h1>
-          <form onSubmit={handleSubmit}
-            className="contact-form">
-            <label htmlFor="name" className="contact-label">Name</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="contact-input"
-              placeholder="Your full name"
-              required
-              value={contactData.name}
-              onChange={handleChange}
-            />
+      <div className="margin-div" id="contact"></div>
+      <div className="contact-container">
+        <h1 className="contact-heading">Contact Us</h1>
+        <form onSubmit={handleSubmit}
+          className="contact-form">
+          <label htmlFor="name" className="contact-label">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="contact-input"
+            placeholder="Your full name"
+            required
+            value={contactData.name}
+            onChange={handleChange}
+          />
 
-            <label htmlFor="phone" className="contact-label">Mobile Number</label>
-            <input
-              type="tel"
-              id="phone"
-              name="phone"
-              className="contact-input"
-              placeholder="Your phone number"
-              required
-              value={contactData.phone}
-              onChange={handleChange}
-            />
+          <label htmlFor="phone" className="contact-label">Mobile Number</label>
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            className="contact-input"
+            placeholder="Your phone number"
+            required
+            value={contactData.phone}
+            onChange={handleChange}
+          />
 
-            <label htmlFor="message" className="contact-label">Message</label>
-            <textarea
-              id="message"
-              name="message"
-              className="contact-input"
-              placeholder="Your message"
-              required
-              value={contactData.message}
-              onChange={handleChange}
-            />
+          <label htmlFor="message" className="contact-label">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            className="contact-input"
+            placeholder="Your message"
+            required
+            value={contactData.message}
+            onChange={handleChange}
+          />
 
-            <button type="submit" className="contact-btn" onClick={handleSubmit}>Send Message</button>
-            <Toaster position="top-right" reverseOrder={false} />
-          </form>
-        </div>
+          <button type="submit" className="contact-btn" onClick={handleSubmit}>Send Message</button>
+          <Toaster position="top-right" reverseOrder={false} />
+        </form>
+      </div>
 
       <footer>
         <div className="about-end-div">

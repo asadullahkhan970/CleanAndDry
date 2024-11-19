8c2fe2472cb883c52fa5d507d7957c078e62c27a
Asadullah-khan596 <asadullahkhan3096@gmail.com>
2024-11-19 13:44:28 +0530

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import Routes and Route
+import App from './App.jsx';
 
+const root = createRoot(document.getElementById('root'));
 
-createRoot(document.getElementById('root')).render(
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes> {/* Wrap your routes in the Routes component */}
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
